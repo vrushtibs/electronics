15ca4f2ace976651403c2f70e9405f6b4aece54b
Vrushti Shah <vrushtibs@gmail.com>
2024-09-25 20:43:10 +0530

    first commit

---
+++ src/Components/Assets/new_collections.js
@@ -0,0 +1,71 @@
+import camera1 from '../Assets/camera1.jpg'
+import camera2 from '../Assets/camera2.png'
+import camera3 from '../Assets/camera3.png'
+import camera4 from '../Assets/camera4.png'
+import camera5 from '../Assets/camera5.jpeg'
+import camera6 from '../Assets/camera6.jpeg'
+import camera7 from '../Assets/camera7.jpeg'
+import camera8 from '../Assets/camera8.jpeg'
+
+let data_product = [
+    {
+        id:1,
+        name:"EOS C80 digital cinema camera",
+        image:camera1,
+        new_price:56490,
+        old_price:72199,
+    },
+    
+    {
+        id:2,
+        name:"CN20x50 IAS H P1",
+        image:camera2,
+        new_price:26490,
+        old_price:32199,
+    },
+    {
+        id:3,
+        name:"RF10-20mm f/4L IS STM",
+        image:camera3,
+        new_price:55900,
+        old_price:65900,
+    },
+    
+    {
+        id:4,
+        name:"110 Million EOS Series Camera",
+        image:camera4,
+        new_price:26490,
+        old_price:32199,
+    },
+    {id:5,
+        name:"Sony ZV-E10 Mirrorless Camera(Black)",
+        image:camera5,
+        new_price:26490,
+        old_price:32199,
+    },
+    {
+        id:6,
+        name:"Sony Cyber-shot DSC-HX99",
+        image:camera6,
+        new_price:11209,
+        old_price:16099,
+    },
+    {
+        id:7,
+        name:"Sony ZV-E10 Mirrorless Camera(White)",
+        image:camera7,
+        new_price:5990,
+        old_price:8790,
+    },
+    
+    {
+        id:8,
+        name:"Sony ZV-E10 Mirrorless Camera",
+        image:camera8,
+        new_price:26490,
+        old_price:32199,
+    },
+];
+
+export default data_product;
